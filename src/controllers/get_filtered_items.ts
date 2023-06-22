4449b87e7b79f2c367a942c1007f739f75c0a547
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-06-22 15:51:39 +0100

    introduce config file and export selected fields

--- src/controllers/get_filtered_items.ts
+++ src/controllers/get_filtered_items.ts
@@ -2,13 +2,33 @@ import { Request, Response } from "express";
 
 import { Item } from '../models/item-model';
 
+import configData from "../config.json";
+
 export const getFilteredItems = async (req: Request, res: Response) => {
+
+  console.log(configData);
+
   if (req) {
     const items = await Item.find( { } );
-    const no_email_items = items.map((it) =>
+    const export_items = items.map((it) =>
       {
+        let out = {};
         const { _id, email, booking_start, booking_end, expireAt, bucket, item} = it;
-        console.log(`EMAIL is ${email}, ITEM is ${item}`)
+        console.log(`EMAIL is ${email}, ITEM is ${item}`);
+        const x = configData.EXPORTS.map((y) => {
+          console.log(`ELEMENT is ${y}`)
+          if (y in it) {
+            const key = `${y}`;
+            console.log("key = ", key);
+            console.log(`FOUND ${y} in object, value is `);
+            console.log(it[key]); 
+            out[key] = it[key];
+          }
+        }); 
+        console.log("X");
+        console.log(out);
+        return out;
+/*
         return {
           "_id": _id,
           "booking_start": booking_start,
@@ -16,10 +36,11 @@ export const getFilteredItems = async (req: Request, res: Response) => {
           "expireAt": expireAt,
           "bucket": bucket,
           "item": item} 
+*/
       } 
     );
     if (items.length) {
-        return res.status(200).json({ success: true, data: no_email_items })
+        return res.status(200).json({ success: true, data: export_items })
     } else {
         return res
             .status(404)
