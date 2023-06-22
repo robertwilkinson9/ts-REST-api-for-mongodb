886f736a0d5c8dfdb0148fd277586efc2ae67bef
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-06-22 16:00:36 +0100

    reduce clutter in code

--- src/controllers/get_filtered_items.ts
+++ src/controllers/get_filtered_items.ts
@@ -5,38 +5,17 @@ import { Item } from '../models/item-model';
 import configData from "../config.json";
 
 export const getFilteredItems = async (req: Request, res: Response) => {
-
-  console.log(configData);
-
   if (req) {
     const items = await Item.find( { } );
     const export_items = items.map((it) =>
       {
-        let out = {};
-        const { _id, email, booking_start, booking_end, expireAt, bucket, item} = it;
-        console.log(`EMAIL is ${email}, ITEM is ${item}`);
-        const x = configData.EXPORTS.map((y) => {
-          console.log(`ELEMENT is ${y}`)
-          if (y in it) {
-            const key = `${y}`;
-            console.log("key = ", key);
-            console.log(`FOUND ${y} in object, value is `);
-            console.log(it[key]); 
-            out[key] = it[key];
+        let filtered = {};
+        configData.EXPORTS.filter((filter) => {
+          if (filter in it) {
+            filtered[filter] = it[filter];
           }
         }); 
-        console.log("X");
-        console.log(out);
-        return out;
-/*
-        return {
-          "_id": _id,
-          "booking_start": booking_start,
-          "booking_end": booking_end, 
-          "expireAt": expireAt,
-          "bucket": bucket,
-          "item": item} 
-*/
+        return filtered;
       } 
     );
     if (items.length) {
