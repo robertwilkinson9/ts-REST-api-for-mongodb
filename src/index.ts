e4857891676af44eb857a9a9cf121f5568f5b505
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-09-05 17:00:53 +0100

    use https for backend

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 import express, { Request, Response } from "express";
+
+const https = require('https');
+const fs = require('fs');
+
+const {MongoClient} = require('mongodb');
+// Never hard-code your credentials, get them from config - in this case, the environment.
+const mongo_uri = '127.0.0.1:27017';
+//const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mongo_uri/${process.env.DB_NAME}?retryWrites=true&writeConcern=majority`;
+const collection = process.env.DB_NAME ? process.env.DB_NAME : "book";
+console.log(collection);
+// const uri = `mongodb://127.0.0.1:27017/${process.env.DB_NAME}?retryWrites=true&writeConcern=majority`;
+const uri = `mongodb://127.0.0.1:27017/${collection}?retryWrites=true&writeConcern=majority`;
+
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
@@ -9,6 +22,24 @@ import configData from "../config/config.json"
 
 const app = express()
 const apiPort = Number(configData.APIPORT);
+
+var dbClient = null;
+var server = null;
+
+async function initDatabase() { 
+    try {
+        const client = new MongoClient(uri, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        });
+
+        await client.connect();
+        return client;
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
@@ -22,4 +53,15 @@ app.get('/', (req: Request, res:Response) => {
 
 app.use('/api', itemRouter)
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+// app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+
+initDatabase().then((client) => {
+    console.log('Database initialized');
+    dbClient = client;
+    server = https.createServer({
+        cert: fs.readFileSync('./certs/localhost.crt'),
+        key: fs.readFileSync('./certs/localhost.key')
+    }, app).listen(
+        443, () => console.log('Server listening on https://localhost:443')
+    );
+});
