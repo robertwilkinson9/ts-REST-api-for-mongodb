88d06fd04abfc83f5b3ad4f2b1d7eda99a7b257f
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-11-06 11:09:01 +0000

    kubernetes files for backend

--- kubernetes/write_frontend_yaml.sh
+++ kubernetes/write_frontend_yaml.sh
@@ -1,11 +1,19 @@
 #/bin/bash
 TYPE=$1
 PORT=$2
-#PORT from head ../../ts-reserve-assets/package.json
 BACKEND_IP=$(kubectl describe service/${TYPE}-backend-service | grep ^IP: | awk '{print $NF}')
 echo beip is $BACKEND_IP
 BACKEND_PORT=$(kubectl describe service/${TYPE}-backend-service | grep TargetPort: | awk '{print $2}' | awk -F/ '{print $1}')
 echo beport is $BACKEND_PORT
+SRC_DIR="../../typescript/ts-reserve-assets/"
+FE_PORT=$(cat ${SRC_DIR}/package.json | jq --raw-output .config.${TYPE})
+echo FE_PORT is $FE_PORT
+END_POINTS=$(kubectl describe service/${TYPE}-backend-service | grep ^Endpoints: | awk '{print $NF}')
+echo endpoints is $END_POINTS
+END_POINT_IP=$(echo ${END_POINTS} | awk -F: '{print $1}')
+echo endpoint_ip is $END_POINT_IP
+END_POINT_PORT=$(echo ${END_POINTS} | awk -F: '{print $2}')
+echo endpoint_port is $END_POINT_PORT
 
 cat << EOF > ${TYPE}-frontend.yaml
 ---
@@ -20,16 +28,16 @@ spec:
     - name: ${TYPE}-frontend
       image: robertwilkinsonwork299/reserve-assets-${TYPE}-frontend
       ports:
-        - containerPort: ${PORT}
+        - containerPort: ${FE_PORT}
       env:
       - name: SSL_CERT
         value: /certs/localhost.crt
       - name: SSL_KEY
         value: /certs/localhost.key
       - name: VITE_API_IP
-        value: "${BACKEND_IP}"
+        value: "${END_POINT_IP}"
       - name: VITE_API_PORT
-        value: "${BACKEND_PORT}"
+        value: "${END_POINT_PORT}"
       - name: VITE_TYPE
         value: "${TYPE}"
 EOF
