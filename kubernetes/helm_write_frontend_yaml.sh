02e4a352f449a371b5a03bbd7263cab7aa8274cd
Robert Wilkinson <robert.wilkinson@raytheon.co.uk>
2023-11-23 15:15:32 +0000

    files needed to get book system to run via helm install - which installs the backend so far

---
+++ kubernetes/helm_write_frontend_yaml.sh
@@ -0,0 +1,56 @@
+#/bin/bash
+TYPE=$1
+PORT=$2
+#BACKEND_IP=$(kubectl describe service/${TYPE}-backend-service | grep ^IP: | awk '{print $NF}')
+#echo beip is $BACKEND_IP
+#BACKEND_PORT=$(kubectl describe service/${TYPE}-backend-service | grep TargetPort: | awk '{print $2}' | awk -F/ '{print $1}')
+#echo beport is $BACKEND_PORT
+SRC_DIR="../../typescript/ts-reserve-assets/"
+FE_PORT=$(cat ${SRC_DIR}/package.json | jq --raw-output .config.${TYPE})
+echo FE_PORT is $FE_PORT
+#END_POINTS=$(kubectl describe service/${TYPE}-backend-service | grep ^Endpoints: | awk '{print $NF}')
+#echo endpoints is $END_POINTS
+END_POINTS=$(kubectl describe service/ra-${TYPE} | grep ^Endpoints: | awk '{print $NF}')
+if [ $? == 0 ]; 
+then
+  echo endpoints is $END_POINTS
+
+  END_POINT_IP=$(echo ${END_POINTS} | awk -F: '{print $1}')
+  echo endpoint_ip is $END_POINT_IP
+  END_POINT_PORT=$(echo ${END_POINTS} | awk -F: '{print $2}')
+  echo endpoint_port is $END_POINT_PORT
+else
+  echo NO endpoints
+
+  END_POINT_IP=$(kubectl describe service/ra-${TYPE} | grep ^IP: | awk '{print $NF}')
+  echo endpoint_ip is $END_POINT_IP
+  END_POINT_PORT=$(kubectl describe service/ra-${TYPE} | grep ^Port: | awk -F/ '{print $1}' | awk '{print $NF}')
+  echo endpoint_port is $END_POINT_PORT
+fi
+
+cat << EOF > ${TYPE}-frontend.yaml
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  name: ${TYPE}-frontend
+  labels:
+    app: web
+spec:
+  containers:
+    - name: ${TYPE}-frontend
+      image: robertwilkinsonwork299/reserve-assets-${TYPE}-frontend
+      ports:
+        - containerPort: ${FE_PORT}
+      env:
+      - name: SSL_CERT
+        value: /certs/localhost.crt
+      - name: SSL_KEY
+        value: /certs/localhost.key
+      - name: VITE_API_IP
+        value: "${END_POINT_IP}"
+      - name: VITE_API_PORT
+        value: "${END_POINT_PORT}"
+      - name: VITE_TYPE
+        value: "${TYPE}"
+EOF
